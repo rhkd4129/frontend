1a643d0de530cfb9e40522c652f1e93f0bc5f8d3
Lee <rhkd4129@naver.com>
2022-03-29 20:45:01 +0900

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from 'antd';
+import 'App.css';
+
+// props
+// import 'antd/dist/antd.css';이렇게 해도됨 
+
+class Counter1 extends React.Component{
+  state={
+    value:this.props.initialValue, //속성값중에 inittal을 value란 이름으로 상태값만듬
+  }
+
+  onClick=()=>{
+    const {value } = this.state
+    this.setState({value:value+1})
+  };
+
+  render(){
+    const {value} = this.state;
+    return (<div>
+      Counter1 : {value}
+      <Button onClick={this.onClick}>+1</Button>
+      </div> 
+      );//jsx문법
+  }
+}
+
+
+class FrulitComponent extends React.Component{
+  render(){
+    
+    return (
+      <div>
+        <h1>좋아하는 과일</h1>
+        <ul>
+          {
+            this.props.fruits.map((name,index)=>(
+               <li key={index}>{name}</li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
+}
 
 function App() {
+  const fruits = ["바나나","사과","달기"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Button type ="primary" onClick={ ()=> console.log("clicked") }>
+        hello Antd
+      </Button>
+      <Counter1 initialValue={10}/>
+      <FrulitComponent fruits={fruits} />
     </div>
   );
 }
